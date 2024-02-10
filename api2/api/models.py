d34eacab6851bd095974e16140122b249dc58e31
akbarhossain <smakbor33@gmail.com>
2024-02-11 00:00:16 +0600

    api2

--- api2/api/models.py
+++ api2/api/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+from rest_framework.authtoken.models import Token
+
 # Create your models here.
 class Task(models.Model):
     title=models.CharField(max_length=50)
@@ -21,3 +26,7 @@ class Role(models.Model):
     username=models.OneToOneField(User, on_delete=models.CASCADE)
     type=models.CharField(max_length=20,choices=TYPE_CHOISE)
 
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
--- api2/api/views.py
+++ api2/api/views.py
@@ -5,11 +5,12 @@ from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
 from .permissions import IsAdminOrReadOnly
-
+from rest_framework.authentication import TokenAuthentication
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsAdminOrReadOnly]
+    authentication_classes=[TokenAuthentication]
+    #permission_classes = [IsAdminOrReadOnly]
     def perform_create(self, serializer):
         username = self.request.user
         serializer.save(username=username)
